b446b4eea5eaf4ef71fdbeaf4e2a430df2d20bb4
Diacius <77890144+Diacius@users.noreply.github.com>
2024-06-20 11:51:33 +0100

    Update ticketcalc with comments, and attempt to fix bugs

--- ticketcalc/ticketcalc.js
+++ ticketcalc/ticketcalc.js
@@ -1,34 +1,41 @@
+// Get the form element
 const form = document.querySelector("#ticket-calculator-form")
 function calculate() {
+    // Collect the form data and get the points in time required
     let formData = new FormData(form);
     currentDate = new Date();
     endDate = new Date('2024-08-31T00:00:00Z');
     // time left in milliseconds
     msLeft = endDate-currentDate;
-    // calculate the number of hours left
-    // convert to seconds then to hours
+    // Convert to seconds then to hours
     secondsLeft = msLeft / 1000
     hoursLeft = secondsLeft / 60 / 60;
     ticketsNeeded = formData.get('ticketsNeeded');
-    percentageOfHoliday = formData.get("pecentageCannotWork")
-    if (formData.get("pecentageCannotWork") !== 0 && formData.get("percentageCannotWork") !== null) {
-        hoursLeft = hoursLeft * percentageOfHoliday;
+    percentageOfHoliday = formData.get("pecentageCanWork");
+    // Check if the user has expressed how many days they cannot work
+    if (!(percentageOfHoliday == 0 || percentageOfHoliday == null || percentageOfHoliday == "")) {
+       // Multiply hours left by multiplier
+       hoursLeft = hoursLeft * percentageOfHoliday        
     }
     else {
         daysMissed = formData.get("daysCannotWork");
-        hoursLeft = hoursLeft - (daysMissed * 24)
+        hoursLeft = hoursLeft - (daysMissed * 24);
     }
-    daysLeft = hoursLeft / 24
+    // 24 hours in a day
+    daysLeft = hoursLeft / 24;
+    // Find the number of tickets needed by subtracting the current number of tickets
     currentTickets = formData.get("tickets");
-    ticketsNeeded = ticketsNeeded - currentTickets
-    console.log("Hours left " + hoursLeft)
-    console.log("Tickets needed" + ticketsNeeded)
-    console.log("Daysleft " + daysLeft)
-    output = document.querySelector('#output')
-    output.innerHTML = (ticketsNeeded / daysLeft)
+    ticketsNeeded = ticketsNeeded - currentTickets;
+    console.log("Hours left " + hoursLeft);
+    console.log("Tickets needed" + ticketsNeeded);
+    console.log("Daysleft " + daysLeft);
+    // Outpout the calculation to the page
+    output = document.querySelector('#output');
+    output.innerHTML = (ticketsNeeded / daysLeft).toFixed(4);
 }
 
 form.addEventListener("submit", (event) => {
+    // prevent a normal form submit and calculate the tickets needed
     event.preventDefault();
     let formData = new FormData(form);
     calculate();
