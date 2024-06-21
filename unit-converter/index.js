cc19f23b16953f6ca8c82ab438d642ddafd1c15d
Diacius <77890144+Diacius@users.noreply.github.com>
2024-06-21 17:10:35 +0100

    Length converter and started work on temp converter

--- unit-converter/index.js
+++ unit-converter/index.js
@@ -1,11 +1,32 @@
 // Length
-in_units = {
-    "inch" : {"metric" : "metre", "conversion_multiplier": "39.37"}
+length_in_units = {
+    "inch" : {"conversion_multiplier": 39.37},
+    "metre": {"conversion_multiplier": 1},
+    "foot" : {"conversion_multiplier": 0.30478512648582749161840902163974},
+    "yard" : {"conversion_multiplier": 0.9144}
+}
+length_out_units = {
+    "metre": {"conversion_multiplier": 1},
+    "inch" : {"conversion_multiplier": 39.370},
+    "foot" : {"conversion_multiplier": 3.28084},
+    "yard" : {"conversion_multiplier": 1.0936}
 }
 function updateLength() {
     inputLength = document.querySelector("#length_in").value;
     inputUnit = document.querySelector("#length_in_unit").value;
     outputLength = document.querySelector("#length_out");
-    outputLength.value = inputLength * in_units[inputUnit].conversion_multiplier
+    outputUnit = document.querySelector("#length_out_unit").value;
+    // Convert to metric
+    toMetric = inputLength * length_in_units[inputUnit].conversion_multiplier
+    // Convert out of metric
+    outValue = toMetric * length_out_units[outputUnit].conversion_multiplier
+    outputLength.value = outValue
+    
+}
+function updateTemperatire() {
+    inputUnit = document.querySelector("#temp_in_unit").value
+    outputUnit = document.querySelector('#temp_out_unit').value
+    inputValue = document.querySelector('#temp_in').value
+    output = document.querySelector('#temp_out')
     
 }
